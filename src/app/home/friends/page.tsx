c7c879867d04ad0b5af97f21a5db3080d12c0c6c
saptarshi dutta <saptarshidutta599@gmail.com>
2023-09-23 18:52:45 +0530

    prisma

---
+++ src/app/home/friends/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='absolute top-5 left-2/4'>
+        
+            friend requiest
+        
+    </div>
+  )
+}
+
+export default page
