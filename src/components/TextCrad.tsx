7d8266bcbb39d2c0eaf24cdfea070245f5f4eead
saptarshi dutta <saptarshidutta599@gmail.com>
2024-01-28 11:41:37 +0530

    text card

--- src/components/TextCrad.tsx
+++ src/components/TextCrad.tsx
@@ -5,10 +5,16 @@ interface props extends chatid {
   chat: chats;
 }
 const TextCrad: React.FC<props> = ({ chat, chatId }) => {
+  const [userid,partnerid]=chatId.split('--');
+  if(chat.senderId===userid)
+  return (<div className="bg-red-300 p-1 ">
+    <div className="">
+    {chat.content}
+    </div>
+  </div>)
   return (
     <div>
       {chat.content}
-      {chatId}
     </div>
   );
 };
